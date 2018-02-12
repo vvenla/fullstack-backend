36d08f783af8a49a5b236e5358ac8786c53e7e32
Venla <venla.viljamaa@helsinki.fi>
2018-02-12 22:23:56 +0200

    tehtävä 3.10 herokuun

--- index.js
+++ index.js
@@ -97,9 +97,9 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 
